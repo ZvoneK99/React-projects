8ac6ccfdc747d38acd2b7a1b551de53320a5ab79
Zvone <zvone.kozull1999@gmail.com>
2025-03-07 15:12:30 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 
 import './style.css';
 import Products from './Components/Products';
+import { useState } from 'react';
+
+
+function clickOnButton() {
+  console.log("Radi");
+}
+
+
 
 function App() {
+  //promjena imena i gumba
+  let [name, setName] = useState("Hello world"); //"use" -> Hook
 
+  function changeName(e) {
+    setName(e.target.value);
+    console.log(name);
+  }
+  //Promjena tax
+  let [tax, setTax] = useState(0);
+
+ 
   return (
     <>
    
     <hr/>
-    <Products tax="20"/>
+    <Products tax={tax}/>
     <hr/>
-
-    </>
+    <Products tax="25"/>
+    <hr/>
+    <Products tax="55"/>
+    <hr/>
+    <button onClick={clickOnButton}>{name}</button>
+    <h1>my name is {name}</h1>
+    <input type='text' placeholder='Unesite novo ime.' onInput={ (e) => {setName(e.target.value)}}/>
+    <br/><hr/>
+    <input type='text'placeholder='Unesite novu vrijednos poreza.' onInput={ (e) => { setTax(e.target.value)}}/>
+        </>
     
   );
 }
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,18 +1,21 @@
+import { useState } from "react";
 
-const products = {
-    "iPhone 14":1000,
-    "iPhone 15":1250,
-    "Samsung S23":1100
-};
 
 function Products(props) {
-    console.log(props.tax)
+
+    let [products, setProducts] = useState({
+        "iPhone 14":1000,
+        "iPhone 15":1250,
+        "Samsung S23":1100
+    });
+
     return (
        <>
             {Object.entries(products)
                 .map(([product, price]) => (
-                <p>{product}: ${price} with tax: ${calculateTax(price, props.tax)}</p>
+                <p key={product}>{product}: ${price} with tax: ${calculateTax(price, props.tax)}</p>
             ))}
+                <button onClick={() => setProducts({})}>Brisanje</button>   {/*arrow funkcija za brisanje*/}
        </>
     )
 };
